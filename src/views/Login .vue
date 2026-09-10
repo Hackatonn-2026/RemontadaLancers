01b93d4e3bd2e45d6a4619d90afba8d5011c5559
eloisamuller <eloisaoliveiramuller@gmail.com>
2026-09-10 15:09:13 -0300

    inicio login

---
+++ src/views/Login .vue
@@ -0,0 +1,16 @@
+<template>
+<main>
+    <div class="login-inicil">
+<h1 class="entar">
+    Entrar na sua conta
+</h1>
+<p class="bem-vindo">Bem-vindo de volta!</p>
+    </div>
+    
+</main>
+</template>
+
+<script setup>
+</script>
+<style scoped>   
+</style>
