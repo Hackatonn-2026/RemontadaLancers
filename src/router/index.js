c48ba937e8e516fcd04b0b74c1b524c499455205
Valentina Damas <damasvalentina9@gmail.com>
2026-09-11 22:47:34 -0300

    feat:addsobrenosview

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import ComoFuncionaView from '../views/ComoFuncionaView.vue'
 import CategoriasView from '../views/CategoriasView.vue'
 import LoginView from '../views/LoginView.vue'
 import CadastroView from '../views/CadastroView.vue'
+import SobreView from '../views/SobreView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -33,6 +34,11 @@ const router = createRouter({
       path: '/cadastro',
       name: 'cadastro',
       component: CadastroView,
+    },
+    {
+      path: '/sobre',
+      name: 'sobre',
+      component: SobreView,
     },
   ],
 })
